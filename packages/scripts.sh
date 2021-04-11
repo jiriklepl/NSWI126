1bc0a7b58c1f25c6d5533e1e2e254f1d7d7b2a3e
jiriklepl <jiriklepl@seznam.cz>
2021-04-11 18:01:07 +0200

    finally

---
+++ packages/scripts.sh
@@ -0,0 +1,19 @@
+printf "Press enter to continue"; read
+
+cd myhello-1.0.0
+debuild -b
+cd ..
+
+printf "Press enter to continue"; read
+
+dpkg-scanpackages . /dev/null > Packages
+gzip -k -f Packages
+
+printf "Press enter to continue"; read
+
+apt-ftparchive release . > Release
+
+printf "Press enter to continue"; read
+
+rm -fr Release.gpg; gpg -abs -o Release.gpg Release
+rm -fr InRelease; gpg --clearsign -o InRelease Release
